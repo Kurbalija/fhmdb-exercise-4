61a027b8f723e8fd25d79ed1664385e5d8f9dd12
fkukcha <ferdaws_kukcha@yahoo.com>
2023-06-14 11:17:49 +0200

    Refactored.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -11,12 +11,8 @@ module at.ac.fhcampuswien.fhmdb {
     exports at.ac.fhcampuswien.fhmdb.model;
     exports at.ac.fhcampuswien.fhmdb.controller;
     exports at.ac.fhcampuswien.fhmdb.exception;
-    exports at.ac.fhcampuswien.fhmdb.provider;
-    exports at.ac.fhcampuswien.fhmdb.factory;
-    exports at.ac.fhcampuswien.fhmdb.sorting;
 
     opens at.ac.fhcampuswien.fhmdb to javafx.fxml;
     opens at.ac.fhcampuswien.fhmdb.model to com.google.gson, ormlite.jdbc;
     opens at.ac.fhcampuswien.fhmdb.controller to javafx.fxml;
-    opens at.ac.fhcampuswien.fhmdb.factory to javafx.fxml;
 }
