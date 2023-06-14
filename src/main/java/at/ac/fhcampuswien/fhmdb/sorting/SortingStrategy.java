3c1fe0ea7d352013c49ab2310dd6a5af2aab4a4f
fkukcha <ferdaws_kukcha@yahoo.com>
2023-06-14 11:18:41 +0200

    Refactored.

--- src/main/java/at/ac/fhcampuswien/fhmdb/sorting/SortingStrategy.java
+++
@@ -1,9 +0,0 @@
-package at.ac.fhcampuswien.fhmdb.sorting;
-
-import at.ac.fhcampuswien.fhmdb.model.Movie;
-
-import java.util.List;
-
-public interface SortingStrategy {
-    void sort(List<Movie> movies);
-}
