b5af7a62d2d72e5a38953898e71d4bd4ef20d8ec
fkukcha <ferdaws_kukcha@yahoo.com>
2023-06-14 11:19:13 +0200

    Added sortAscending and sortDescending methods.

--- src/main/java/at/ac/fhcampuswien/fhmdb/sorting/Unsorted.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/sorting/Unsorted.java
@@ -1,12 +1,24 @@
 package at.ac.fhcampuswien.fhmdb.sorting;
 
+import at.ac.fhcampuswien.fhmdb.controller.HomeController;
 import at.ac.fhcampuswien.fhmdb.model.Movie;
 
+import java.util.Comparator;
 import java.util.List;
 
-public class Unsorted implements SortingStrategy {
+public class Unsorted extends SortedState {
+    public Unsorted(HomeController homeController) {
+        super(homeController);
+    }
+
     @Override
-    public void sort(List<Movie> movies) {
-        // No sorting needed
+    public void sortAscending(List<Movie> movies) {
+        movies.sort(Comparator.comparing(Movie::getTitle));
+        homeController.setSortState(new Ascending(homeController));
+    }
+
+    public void sortDescending(List<Movie> movies){
+        movies.sort(Comparator.comparing(Movie::getTitle).reversed());
+        homeController.setSortState(new Descending(homeController));
     }
 }
