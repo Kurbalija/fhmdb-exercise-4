627831130281651396ec963a4ec159ab256a1c5d
fkukcha <ferdaws_kukcha@yahoo.com>
2023-06-14 11:12:49 +0200

    Implemented the Ascending order.

--- src/main/java/at/ac/fhcampuswien/fhmdb/sorting/Ascending.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/sorting/Ascending.java
@@ -1,11 +1,24 @@
 package at.ac.fhcampuswien.fhmdb.sorting;
 
-import java.util.Comparator;
+import at.ac.fhcampuswien.fhmdb.controller.HomeController;
+import at.ac.fhcampuswien.fhmdb.model.Movie;
+
+import java.util.Collections;
 import java.util.List;
 
-public class Ascending implements SortingStrategy {
+public class Ascending extends SortedState {
+    public Ascending(HomeController homeController) {
+        super(homeController);
+    }
+
     @Override
-    public void sort(List<at.ac.fhcampuswien.fhmdb.model.Movie> movies) {
-        movies.sort(Comparator.comparing(at.ac.fhcampuswien.fhmdb.model.Movie::getTitle));
+    public void sortDescending(List<Movie> movies) {
+        Collections.reverse(movies);
+        homeController.setSortState(new Descending(homeController));
+    }
+
+    @Override
+    public void sortAscending(List<Movie> movies){
+
     }
 }
