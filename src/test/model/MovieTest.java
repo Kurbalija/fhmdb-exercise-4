2b61fd6bb361444cac0755341ca1dcd45cf7c89a
fkukcha <ferdaws_kukcha@yahoo.com>
2023-06-14 11:39:28 +0200

    Refactored.

--- src/test/model/MovieTest.java
+++ src/test/model/MovieTest.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 
 public class MovieTest {
     @Test
-    public void does_create() {
+    public void create_movie_object_does_not_throw_exception() {
         assertDoesNotThrow(() -> {
             Movie mov = new Movie("1", "Test", new ArrayList<>(), 2021, "Test", "http://test.com",
                     120, new ArrayList<String>(), new ArrayList<String>(), new ArrayList<String>(), 10.0);
@@ -16,7 +16,7 @@ public class MovieTest {
     }
 
     @Test
-    public void invalid_id(){
+    public void invalid_id() {
         assertThrows(IllegalArgumentException.class, () -> {
             Movie mov = new Movie(null, "Test", new ArrayList<String>(), 2021, "Test", "http://test.com",
                     120, new ArrayList<String>(), new ArrayList<String>(), new ArrayList<String>(), 10.0);
@@ -24,7 +24,7 @@ public class MovieTest {
     }
 
     @Test
-    public void invalid_genres(){
+    public void invalid_genres() {
         assertThrows(IllegalArgumentException.class, () -> {
             Movie mov = new Movie("1", "Test", null, 2021, "Test", "http://test.com",
                     120, new ArrayList<String>(), new ArrayList<String>(), new ArrayList<String>(), 10.0);
@@ -32,7 +32,7 @@ public class MovieTest {
     }
 
     @Test
-    public void invalid_releaseYear(){
+    public void invalid_releaseYear() {
         assertThrows(IllegalArgumentException.class, () -> {
             Movie mov = new Movie("1", "Test", new ArrayList<String>(), 2024, "Test", "http://test.com",
                     120, new ArrayList<String>(), new ArrayList<String>(), new ArrayList<String>(), 10.0);
@@ -40,7 +40,7 @@ public class MovieTest {
     }
 
     @Test
-    public void invalid_releaseYear_2(){
+    public void invalid_releaseYear_2() {
         assertThrows(IllegalArgumentException.class, () -> {
             Movie mov = new Movie("1", "Test", new ArrayList<String>(), 1959, "Test", "http://test.com",
                     120, new ArrayList<String>(), new ArrayList<String>(), new ArrayList<String>(), 10.0);
@@ -48,7 +48,7 @@ public class MovieTest {
     }
 
     @Test
-    public void invalid_description(){
+    public void invalid_description() {
         assertThrows(IllegalArgumentException.class, () -> {
             Movie mov = new Movie("1", "Test", new ArrayList<String>(), 2021, null, "http://test.com",
                     120, new ArrayList<String>(), new ArrayList<String>(), new ArrayList<String>(), 10.0);
@@ -56,7 +56,7 @@ public class MovieTest {
     }
 
     @Test
-    public void invalid_imageUrl(){
+    public void invalid_imageUrl() {
         assertThrows(IllegalArgumentException.class, () -> {
             Movie mov = new Movie("1", "Test", new ArrayList<String>(), 2021, "Test", null,
                     120, new ArrayList<String>(), new ArrayList<String>(), new ArrayList<String>(), 10.0);
@@ -64,7 +64,7 @@ public class MovieTest {
     }
 
     @Test
-    public void invalid_lengthInMinutes(){
+    public void invalid_lengthInMinutes() {
         assertThrows(IllegalArgumentException.class, () -> {
             Movie mov = new Movie("1", "Test", new ArrayList<String>(), 2021, "Test", "http://test.com",
                     -1, new ArrayList<String>(), new ArrayList<String>(), new ArrayList<String>(), 10.0);
@@ -72,7 +72,7 @@ public class MovieTest {
     }
 
     @Test
-    public void invalid_directors(){
+    public void invalid_directors() {
         assertThrows(IllegalArgumentException.class, () -> {
             Movie mov = new Movie("1", "Test", new ArrayList<String>(), 2021, "Test", "http://test.com",
                     120, null, new ArrayList<String>(), new ArrayList<String>(), 10.0);
@@ -80,7 +80,7 @@ public class MovieTest {
     }
 
     @Test
-    public void invalid_writers(){
+    public void invalid_writers() {
         assertThrows(IllegalArgumentException.class, () -> {
             Movie mov = new Movie("1", "Test", new ArrayList<String>(), 2021, "Test", "http://test.com",
                     120, new ArrayList<String>(), null, new ArrayList<String>(), 10.0);
@@ -88,7 +88,7 @@ public class MovieTest {
     }
 
     @Test
-    public void invalid_mainCast(){
+    public void invalid_mainCast() {
         assertThrows(IllegalArgumentException.class, () -> {
             Movie mov = new Movie("1", "Test", new ArrayList<String>(), 2021, "Test", "http://test.com",
                     120, new ArrayList<String>(), new ArrayList<String>(), null, 10.0);
@@ -96,7 +96,7 @@ public class MovieTest {
     }
 
     @Test
-    public void invalid_rating(){
+    public void invalid_rating() {
         assertThrows(IllegalArgumentException.class, () -> {
             Movie mov = new Movie("1", "Test", new ArrayList<String>(), 2021, "Test", "http://test.com",
                     120, new ArrayList<String>(), new ArrayList<String>(), new ArrayList<String>(), -1.0);
@@ -104,7 +104,7 @@ public class MovieTest {
     }
 
     @Test
-    public void invalid_rating_2(){
+    public void invalid_rating_2() {
         assertThrows(IllegalArgumentException.class, () -> {
             Movie mov = new Movie("1", "Test", new ArrayList<String>(), 2021, "Test", "http://test.com",
                     120, new ArrayList<String>(), new ArrayList<String>(), new ArrayList<String>(), 11.0);
