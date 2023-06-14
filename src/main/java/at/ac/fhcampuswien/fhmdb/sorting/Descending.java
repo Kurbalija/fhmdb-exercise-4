9a909deee60aeed354ec459fcdd97d8f581cee62
fkukcha <ferdaws_kukcha@yahoo.com>
2023-06-14 11:14:50 +0200

    Implemented the descending order.

--- src/main/java/at/ac/fhcampuswien/fhmdb/sorting/Descending.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/sorting/Descending.java
@@ -1,13 +1,23 @@
 package at.ac.fhcampuswien.fhmdb.sorting;
 
+import at.ac.fhcampuswien.fhmdb.controller.HomeController;
 import at.ac.fhcampuswien.fhmdb.model.Movie;
 
-import java.util.Comparator;
+import java.util.Collections;
 import java.util.List;
 
-public class Descending implements SortingStrategy {
-    @Override
-    public void sort(List<Movie> movies) {
-        movies.sort(Comparator.comparing(Movie::getTitle).reversed());
+public class Descending extends SortedState {
+    public Descending(HomeController homeController) {
+        super(homeController);
     }
+
+    @Override
+    public void sortDescending(List<Movie> movies) {}
+
+    @Override
+    public void sortAscending(List<Movie> movies){
+        Collections.reverse(movies);
+        homeController.setSortState(new Ascending(homeController));
+    }
+
 }
